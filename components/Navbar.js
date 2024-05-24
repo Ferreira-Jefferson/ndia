31847fed7d758e730a30e5fd0e0b5a6b7e1ea5b3
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2024-05-24 21:51:33 +0000

    fix: navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { useAppContext } from "../context/AppContext";
 
 const Navbar = () => {
-  const { images, transcriptions } = useAppContext();
+  const { images, transcriptions, textEmb } = useAppContext();
 
   return (
     <nav className="bg-gray-800 p-4">
@@ -20,14 +20,14 @@ const Navbar = () => {
             </Link>
           </li>
         )}
-        {images.length > 0 && (
+        {transcriptions.length > 0 && (
           <li>
             <Link href="/transcriptions" className="text-white">
               Transcrições
             </Link>
           </li>
         )}
-        {transcriptions.length > 0 && (
+        {textEmb.length > 0 && (
           <li>
             <Link href="/chat" className="text-white">
               Chat
